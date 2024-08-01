10f716fd9c5d853ba74ee93e40f9be6c70a46e2c
pujatate <puja.tate@ebixcash.com>
2024-08-01 14:25:24 +0530

    name changed

--- src/main/java/com/example/crudapp/StudentApplication.java
+++ src/main/java/com/example/crudapp/StudentApplication.java
@@ -8,7 +8,7 @@ public class StudentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
-		System.out.println("Kiran");
+		System.out.println("Puja Tate");
 	}
 
 }
