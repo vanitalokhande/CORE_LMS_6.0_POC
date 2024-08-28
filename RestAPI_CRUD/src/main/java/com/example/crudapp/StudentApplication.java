7235f98ca0b85ea00b27fd7a9fe436d9590661c1
abhijeetmagdum <abhijeet.magdum@ebixcash.com>
2024-08-28 14:18:43 +0530

    Added Backend

---
+++ RestAPI_CRUD/src/main/java/com/example/crudapp/StudentApplication.java
@@ -0,0 +1,14 @@
+package com.example.crudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+		System.out.println("Kiran");
+	}
+
+}
