3ab7d82f45853d3ea82aac5f85b46c0febb20f22
abhijeetmagdum <121095991+abhijeetmagdum@users.noreply.github.com>
2024-08-28 12:56:00 +0530

    Delete src directory

--- src/main/java/com/example/crudapp/exception/StudentNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.crudapp.exception;
-
-
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/crudapp/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crudapp.repository;
-
-import com.example.crudapp.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
